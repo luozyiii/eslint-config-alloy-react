32388d4da78608591706ddba512f686c92a6b8c7
罗智毅 <zhiyiluo@drgou.com>
2021-03-03 15:24:44 +0800

    you hua

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 // react 示例 https://alloyteam.github.io/eslint-config-alloy/?language=zh-CN&rule=react
-
+let a = 1;
+if (a) {
+  console.log(a);
+}
 class Foo extends React.Component {
   static defaultprops = {};
   componentdidupdate() {}
